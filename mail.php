01a9febb915eb69a50350a707bb6b30ff3187a0f
mgcreations <Jazz.H@JaZZ.H>
2020-04-29 14:25:48 +0200

    Styling contact form and added php support

---
+++ mail.php
@@ -0,0 +1,23 @@
+<?php
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $quote = $_POST['quote'];
+    $message = $_POST['message'];
+
+    $formcontent = "From : $name \n Email: $email \n Phone: $phone \n Message: $message";
+
+    $recipient = "info@lhd1.co.za";
+    $subject = "Quote Request for $quote";
+    $mailheader = "From: $email \r\n";
+
+    mail($recipient,$subject,$formcontent, $mailheader) or die("Error!");
+		
+		if (!$_POST['submit_pay'] == 'Submit_pay') 
+		{
+			header('Location: http://www.lhdsportswear.co.za/thank_you.html');
+			
+		}
+
+    ?>
